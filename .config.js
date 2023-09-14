98bbd62335ad1a2b18565b333ef7e1415e02b97e
StephenJBall <stephenjb94@gmail.com>
2023-09-14 10:33:15 +0100

    Fleshed out directory structure

---
+++ .config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    PORT: process.env.PORT,
+
+    DB: {
+        PG_PORT: process.env.PG_PORT,
+        PG_HOST: process.env.PG_HOST,
+        PG_DATABASE: process.env.PG_DATABASE,
+        PG_USER: process.env.PG_USER,
+        PG_PASSWORD: process.env.PG_PASSWORD
+    }
+    
+}
