bda1d2c3796a45c3707c62dea8cb6ff5fda8e418
StephenJBall <stephenjb94@gmail.com>
2023-09-14 12:23:10 +0100

    added server init

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const express = require('express'); 
 const app = express(); 
+
+const { PORT } = require('./config'); 
+
+async function server() {
+
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`); 
+    })
+
+}
+
+server(); 
