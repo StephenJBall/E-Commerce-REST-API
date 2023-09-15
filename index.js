d21b437f06334bdeb28bac393751a1647616b9e1
StephenJBall <stephenjb94@gmail.com>
2023-09-15 15:36:11 +0100

    Updated index for middleware integration

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express'); 
 const app = express(); 
+const middleware = require('./middleware'); 
 
 const { PORT } = require('./config'); 
 
 async function server() {
+
+    middleware(app); 
 
     app.listen(PORT, () => {
         console.log(`Listening on port ${PORT}`); 
