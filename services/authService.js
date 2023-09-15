72eb3b1d2db635dee81dbb393dec6fa8100dfd0b
StephenJBall <stephenjb94@gmail.com>
2023-09-15 15:57:02 +0100

    Added authentication service; login, register

---
+++ services/authService.js
@@ -0,0 +1,39 @@
+const httpError = require('http-errors'); 
+const User = require('../models/user'); 
+
+module.exports.register = async (data) => {
+    
+    const { email } = data; 
+
+    try {
+        const user = await User.findByEmail(email); 
+
+        if(user) {
+            throw httpError(409, 'This email is already in use'); 
+        } 
+
+        return await User.create(data); 
+    } catch (err) {
+        throw (err); 
+    }
+}
+
+module.exports.login = async (data) => {
+    const { email, password } = data; 
+
+    try {
+        const user = await User.findByEmail(email); 
+
+        if(!user) {
+            throw httpError(404, 'Email or password is incorrect'); 
+        }
+
+        if(user.password !== password) {
+            throw httpError(409, 'Email or password is incorrct'); 
+        }
+
+        return user; 
+    } catch(err) {
+        throw (err); 
+    }
+}
