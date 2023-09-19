7ca86ce8d7655fe2a8bcaf363343e1f89571d0be
StephenJBall <stephenjb94@gmail.com>
2023-09-19 15:27:47 +0100

    Added cart router to index

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const userRouter = require('./userRoute');
 const productRouter = require('./productRoute');
 const orderRouter = require('./orderRoute');
 const authRouter = require('./authRoute'); 
+const cartRouter = require('./cartRoute');
 
 module.exports = (app, passport) => {
     userRouter(app); 
     productRouter(app); 
     orderRouter(app); 
     authRouter(app, passport); 
+    cartRouter(app); 
 }
