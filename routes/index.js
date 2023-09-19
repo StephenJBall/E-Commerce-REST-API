065ff299fce369686ce31364b74b1c647fd01937
StephenJBall <stephenjb94@gmail.com>
2023-09-19 14:42:53 +0100

    Added order route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const passport = require('../middleware/passport'); 
 const userRouter = require('./userRoute');
 const productRouter = require('./productRoute');
+const orderRouter = require('./orderRoute');
 
 module.exports = (app) => {
     userRouter(app); 
     productRouter(app); 
+    orderRouter(app); 
 }
---
+++ routes/orderRoute.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.router();
+const Order = require('../services/orderService'); 
+
+module.exports = (app) => {
+
+    app.use('/orders', router); 
+
+    router.get('/', async (req, res, next) => {
+
+        try {
+            const { id } = req.user;
+
+            const response = await Order.getOrder(id); 
+
+            res.status(200).send(response); 
+        } catch(err) {
+            next (err);
+        }
+
+    })
+
+    router.get('/:orderId', async (req, res, next) => {
+
+        try  {
+            const { orderId } = req.params;
+
+            const response = await Order.getOrder(orderId); 
+
+            res.status(200).send(response);
+        } catch(err) {
+            next (err);
+        }
+
+    })
+
+    router.get('/:userId', async (req, res, next) => {
+        
+        try {
+            const { userId } = req.params; 
+
+            const response = await Order.getOrderByUser(userId);
+
+            res.status(200).send(response);
+        } catch(err) {
+            next (err);
+        }
+    })
+
+}
